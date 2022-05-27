d76e473e2a7531ec74e9dff873716ac77a57433a
ajspurr <37152191+ajspurr@users.noreply.github.com>
2022-05-27 14:58:46 -0400

    Update covid_eda3.py

--- covid_eda3.py
+++ covid_eda3.py
@@ -29,17 +29,17 @@ covid_patient_df.patient_id.nunique()
 del patients
 
 # How many COVID patients have died?
-covid_patients_deceased = covid_patient_df[covid_patient_df.DEATHDATE.notna()].patient_id
+covid_patients_deceased = covid_patient_df[covid_patient_df.DEATHDATE.notna()]
 # 3,641
 
-# ====================================================
+# ==============================================================================
 # Explore how COVID patients died
-# ====================================================
+# ==============================================================================
 # Read in encounters, filter for COVID patients who died 
 encounters = pd.read_csv(data_dir+'encounters.csv', usecols=['Id', 'START', 'STOP', 'PATIENT', 'ENCOUNTERCLASS', 'DESCRIPTION', 'REASONDESCRIPTION'])
 encounters.rename(columns={'PATIENT':'patient_id'}, inplace=True)
 
-encounters_deceased_covid_patients = pd.merge(covid_patients_deceased, encounters, on ='patient_id', how ="left")
+encounters_deceased_covid_patients = pd.merge(covid_patients_deceased.patient_id, encounters, on ='patient_id', how ="left")
 
 # Save disk space
 del encounters
@@ -57,7 +57,8 @@ deceased_covid_patients_w_death_cert.patient_id.nunique()
 # 3,601 unique patient_ids for 3,601 death certifications, confirms that there are no duplicate death certs
 
 # Explore REASONDESCRIPTION of Death Certification encounters 
-deceased_covid_patients_w_death_cert.REASONDESCRIPTION.value_counts()
+print("========== Death Certification 'Reason Description' ==========")
+print(deceased_covid_patients_w_death_cert.REASONDESCRIPTION.value_counts())
 '''
 'REASONDESCRIPTION' for the 3,601 patients with a 'Death Certification' encounter
 COVID-19                                                  3518
@@ -83,16 +84,94 @@ Chronic obstructive bronchitis (disorder)                    1
 '''
 # 3518 were documented death due to COVID
 
-# ==========================
+# ====================================================
 # Explore the patients with history of COVID who do not have a Death Certification Record
-# ==========================
+# ====================================================
 encounters_deceased_covid_patients.patient_id.unique()
 
 deceased_covid_patients_without_death_cert = [patient for patient in covid_patients_deceased if patient not in deceased_covid_patients_w_death_cert.patient_id.tolist()]
 len(deceased_covid_patients_without_death_cert)
 # 40 patients, which = 3,641 covid patients deceased minus 3,601 with death certificate
 
+# I'll start by exploring one of these patients
+deceased_covid_patients_without_death_cert_df = pd.merge(pd.Series(deceased_covid_patients_without_death_cert, name="patient_id"), covid_patients_deceased, on='patient_id', how='left')
+
+# Add deathdate column in datetime format
+deceased_covid_patients_without_death_cert_df['DEATHDATE']
+deceased_covid_patients_without_death_cert_df['deathdate_dt'] = pd.to_datetime(deceased_covid_patients_without_death_cert_df['DEATHDATE'], format='%Y-%m-%d')
+
+# ==========================
+# Explore patient 1
+# ==========================
+patient1_deathdate = deceased_covid_patients_without_death_cert_df.iloc[0].deathdate_dt
+
+patient1_id = deceased_covid_patients_without_death_cert_df.iloc[0].patient_id
+
+patient1_encounters = encounters_deceased_covid_patients[encounters_deceased_covid_patients['patient_id']==patient1_id]
+patient1_encounters['START']
+
+# Convert dates to datetime objects
+patient1_encounters['start_dt'] = pd.to_datetime(patient1_encounters['START'], format='%Y-%m-%dT%H:%M:%SZ')
+patient1_encounters['stop_dt'] = pd.to_datetime(patient1_encounters['STOP'], format='%Y-%m-%dT%H:%M:%SZ')
+
+patient1_encounters = patient1_encounters.sort_values(by='stop_dt')
+patient1_encounters.tail(1).T
+
+# death date = 2020-05-17
+# most recent encounter = 2020-05-15 to 2020-05-16, inpatient, reason = heart failure
+
+# Date of covid diagnosis?
+conditions_df = pd.read_csv(data_dir+'conditions.csv')
+patient1_conditions = conditions_df[conditions_df['PATIENT']==deceased_covid_patients_without_death_cert_df.iloc[0].patient_id]
+patient1_covid_condition = patient1_conditions[patient1_conditions['DESCRIPTION']=='COVID-19']
+patient1_covid_start = patient1_covid_condition.START
+patient1_covid_stop = patient1_covid_condition.STOP
+# COVID start 2020-03-10
+# COVID stop 2020-04-11
+
+# Encounter associated with covid diagnosis
+covid_encounter = patient1_encounters[patient1_encounters['Id']==patient1_covid_condition.ENCOUNTER.iloc[0]]
+covid_encounter.T
+# Encounter does NOT specify COVID, DESCRIPTION = Encounter for symptom (procedure), REASONDESCRIPTION = NaN
+# Encounter start/stop = 2020-03-10
+
+patient1_encounters.REASONDESCRIPTION.value_counts()
+patient1_encounters.DESCRIPTION.value_counts()
+# No encounters with COVID information
+
+# Check if there is an associated procedure for this encounter
+procedures = pd.read_csv(data_dir+'procedures.csv')
+covid_procedure = procedures[procedures['ENCOUNTER']==covid_encounter.Id.iloc[0]]
+covid_procedure.T
+# DESCRIPTION = face mask, REASONDESCRIPTION = Suspected COVID-19
+
+# Look up careplans for the patient
+careplans = pd.read_csv(data_dir+'careplans.csv')
+
+patient1_careplans = careplans[careplans.PATIENT==patient1_id]
+
+patient1_careplans.columns
+
+patient1_careplans[['START', 'STOP']]
+patient1_careplans[['DESCRIPTION', 'REASONDESCRIPTION']]
+patient1_careplans[['CODE', 'REASONCODE']]
+# DESCRIPTION for Suspected COVID-19 and COVID-19 REASONDESCRIPTIONs is 'Infectious disease care plan (record artifact) '
+# I assume that is an outpatient careplan, I can check encounters to be sure
 
 
+covid_careplan = patient1_careplans[patient1_careplans.REASONDESCRIPTION=='COVID-19']
+covid_careplan.START
+covid_careplan.STOP
+covid_careplan.DESCRIPTION
+
+# To summarize:
+# Had COVID start and stop dates, most recent encounter was inpatient for heart failure, which was a month after covid stop date and ended
+# one day before death date
+# COVID encounter was just an 'Encounter for symptom (procedure)' and 'ambulatory' for ENCOUNTERCLASS
+# COVID careplan was called 'Infectious disease care plan (record artifact)' and the dates lined up with diagosis start and stop dates
 
 
+# ==========================
+# For all patients without death certification encounter, what is the amount of time between their
+# most recent encounter and their death date?
+# ==========================
